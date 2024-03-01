12ae6b79010b6990cb787793a5ed5192e6b070b1
Ajryan34 <ajr00026@mix.wvu.edu>
2024-03-01 13:29:45 -0500

    Updated App, added Card and Navbar files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import './Card.js';
+import './Navbar.js';
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App">    
+          <h1>
+              CS 230L
+          </h1>
+          <h2>
+              Section - 001
+          </h2>
+          <p>
+              WVU ID: 800377113
+          </p>
+          <p>
+              Hi I am Avery J Ryan
+          </p>
+          
     </div>
   );
 }
